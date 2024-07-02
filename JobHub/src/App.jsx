c3167b5c82dee893cfc24d040e379e55a7148cc2
iuliangeorgepetre <petregeorge19@stud.ase.ro>
2024-07-02 21:41:46 +0300

    fix authentication

--- JobHub/src/App.jsx
+++ JobHub/src/App.jsx
@@ -1,62 +1,86 @@
-import React, { useState, useEffect } from 'react';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
-import { BrowserRouter as Router, Routes, Route, Link, Navigate, useNavigate } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  Navigate,
+  useNavigate,
+} from "react-router-dom";
 
-import Login from './Components/LoginSignup/Login';
-import SignUp from './Components/LoginSignup/Signup';
-import Dashboard from './Components/Home';
-import Navbar from './Components/Navbar';
+import Login from "./Components/LoginSignup/Login";
+import SignUp from "./Components/LoginSignup/Signup";
+import Dashboard from "./Components/Home";
+import Navbar from "./Components/Navbar";
+
+import Admin from "./Admin";
+import { jwtDecode } from "jwt-decode";
 
 function App() {
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
-    const [jwtToken, setJwtToken] = useState(null);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  const [user, setUser] = useState(null);
+  const navigate = useNavigate();
 
-    useEffect(() => {
-        const token = localStorage.getItem('jwtToken');
-        if (token) {
-            setJwtToken(token);
-            setIsLoggedIn(true);
-        }
-    }, []);
+  useEffect(() => {
+    const token = localStorage.getItem("jwtToken");
+    if (token) {
+      setIsLoggedIn(true);
+      setUser(jwtDecode(token));
+    }
+  }, []);
 
-    const handleLoginSuccess = (token) => {
-        console.log('Login successful. Token:', token); // Debug statement
-        localStorage.setItem('jwtToken', token);
-        setJwtToken(token);
-        setIsLoggedIn(true);
-        window.location.href = '/home';
-        // navigate('/home');
-    };
+  const handleLoginSuccess = (token) => {
+    console.log("Login successful. Token:", token); // Debug statement
+    localStorage.setItem("jwtToken", token);
 
-    const handleLogout = () => {
-        localStorage.removeItem('jwtToken');
-        setIsLoggedIn(false);
-        setJwtToken(null);
-        window.location.href = '/sign-in';
-    };
+    setIsLoggedIn(true);
+    setUser(jwtDecode(token));
+    navigate("/home"); //using navigate as it's RR6 way of redirecting
+  };
 
-    console.log('App component re-rendered. isLoggedIn:', isLoggedIn); // Debug statement
-    console.log(localStorage.getItem("jwtToken"))
+  const handleLogout = () => {
+    localStorage.removeItem("jwtToken");
+    setIsLoggedIn(false);
 
-    return (
-        <Router>
-            <div className="App">
-                <Navbar isLoggedIn={isLoggedIn} handleLogout={handleLogout} />
-                <Routes>
-                    {/* <Route
-                        exact
-                        path="/home"
-                        element={isLoggedIn ? <Navigate to="/home" /> : <Navigate to="/sign-in" />}
-                    /> */}
-                    <Route path="/sign-in" element={<Login handleLoginSuccess={handleLoginSuccess} />} />
-                    <Route path="/sign-up" element={<SignUp />} />
-                    <Route path="/" element={isLoggedIn ?   <Navigate to="/home" /> : <Navigate to="/sign-in" />} />
-                </Routes>
-            </div>
-        </Router>
-    );
+    navigate("/sign-in"); // Use navigate for programmatic navigation
+  };
+
+  console.log("App component re-rendered. isLoggedIn:", isLoggedIn); // Debug statement
+  console.log(localStorage.getItem("jwtToken"));
+
+  return (
+    <div className="App">
+      <Navbar isLoggedIn={isLoggedIn} handleLogout={handleLogout} />
+      <Routes>
+        <Route
+          index
+          element={
+            isLoggedIn ? (
+              <Navigate to="/home" replace />
+            ) : (
+              <Navigate to="/sign-in" replace />
+            )
+          }
+        />
+        <Route path="/home" element={<Dashboard />} />
+        <Route
+          path="/sign-in"
+          element={
+            isLoggedIn ? (
+              <Navigate to="/home" />
+            ) : (
+              <Login handleLoginSuccess={handleLoginSuccess} />
+            )
+          }
+        />
+        <Route path="/admin" element={<Admin user={user} />} />
+        <Route path="/sign-up" element={<SignUp />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
